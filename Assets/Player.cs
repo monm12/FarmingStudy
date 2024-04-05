79459eb3800b27a2e4479ba05926c64589875f67
TaeGyun_Kim <134123228+monm12@users.noreply.github.com>
2024-04-06 03:02:49 +0900

    Basic

---
+++ Assets/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Actor
+{
+  public string move()
+    {
+        return "움직입니다";
+    }
+}
--- Assets/scriptEx 1.cs
+++ Assets/scriptEx 1.cs
@@ -4,15 +4,159 @@ using UnityEngine;
 
 public class scriptEx1 : MonoBehaviour
 {
+    int health = 100;
+    int level = 5;
     // Start is called before the first frame update
     void Start()
     {
+        // b03 Log 찍기
+        Debug.Log("hi");
+
+        // b04 C# 기초
+        // Data Type
+        int i = 10;
+        float j = 1.0f;
+        string k = "It is String";
+        bool l = true; // or false;
+
+        // Reference Type
+        string[] name = { "Kim", "Lee", "Park" };
+        Debug.Log(name[0]); // 0번 인덱스의 데이터 값 참조
+        Debug.Log(name[2]);
+
+        int[] level = new int[3]; // 길이가 4인 배열 선언
+        level[0] = 10;
+        level[1] = 20;
+        level[2] = 30;
+        level[3] = 40;
+        Debug.Log(level[0]); // 10
+        Debug.Log(level[1]); // 20
+
+        List<string> items = new List<string>(); // 리스트 선언
+        items.Add("new String 1"); // 0번 인덱스
+        items.RemoveAt(0); // 0번 인덱스 값 삭제
+
+        // 연산자 +, -, *, /, %
+
+        int exp = 1500;
+        exp += 500;
+        exp -= 300;
+        exp *= 2;
+
+        string playerName = "KIM";
+        string title = "전설의";
+        Debug.Log(title + "" + playerName);
+
+        // 비교 연산자
+        // == , !=, >=, <=, <, >
+        // 논리연산자
+        // && == and , || == or , 삼항연산자 ? A : B - true A, false B
+        bool isBadCondition = false;
+        string condition = isBadCondition ? "bad" : "goot";
+
+        // 조건 if - else, else if
+        if (exp == 40)
+        {
+            Debug.Log("it is 40");
+        }
+        else if (exp == 50){
+            Debug.Log("exp is 50");
+        }
+        else
+        {
+            Debug.Log("it is false");
+        }
+
+        switch (level[0])
+        {
+            case 0:
+                Debug.Log("lvl 1");
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default: // 모두 해당 x 일 때 실행
+                break;
+        }
+
+        // 반복
         
+        while(health > 0)
+        {
+            health--;
+            if (health < 0)
+            {
+                Debug.Log("사망");
+            }
+            if(health == 10)
+            {
+                Debug.Log("포션 사용");
+                break;
+            }
+        }
+
+        for (int count = 0; count < 10; count++)
+        {
+            health++;
+            Debug.Log("치료중" + health);
+
+        }
+        for (int idx = 0; idx < name.Length; idx++) // 배열-Length , List- Count
+        {
+            Debug.Log("이름:" + name[idx]);
+        }
+
+        // foreach - 파이썬의 for i in list 와 비슷
+        foreach (string names in name) // name 배열의 인덱스를 하나씩 names 변수로 가져옴
+        {
+
+        }
+
+        health = Heal(health);
+
+        for (int idx = 0; idx < name.Length; idx++)
+        {
+            Debug.Log("이름:" + name[idx] + "레벨:");// + Battle(name[idx]);
+        }
+
+        // 클래스
+        //Actor player = new Actor(); // 객체 생성
+        Player player = new Player(); // Actor 상속
+        // 접근자 public, private
+
+        player.id = 0;
+        player.name = "kim";
+        player.Talk();
+
+    }
+    // 함수, 메소드
+    int Heal(int health)
+    {
+        health += 10;
+        Debug.Log("힐을 받습니다" + health); 
+        return health;
+    }
+    void Heal()
+    {
+        health += 10;
+        Debug.Log("힐을 받습니다" + health);
     }
 
-    // Update is called once per frame
-    void Update()
+    string Battle(int lvl)
     {
-        
+        string result;
+        if (level >= lvl)
+        {
+            result = "이겼습니다";
+        }
+        else
+        {
+            result = " 졌습니다";
+        }
+        return result;
     }
+
 }
